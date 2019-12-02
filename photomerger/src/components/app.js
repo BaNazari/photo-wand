8300d34c7fd3b3ba6fc655d27dd4b48d152dc9a7
baran <fabanaza@gmail.com>
2019-12-02 23:08:26 +0330

    before github push

--- photomerger/src/components/app.js
+++ photomerger/src/components/app.js
@@ -19,12 +19,12 @@ class App extends React.Component {
             <div className="whole-page container">
                 <div class="w-100"></div>
                 
-                <div className="wrap row  justify-content-md-center">
+                <div className="wrap row  justify-content-center">
                     <div className="col-12 help">
                         <p>help</p>
                     </div>
                 </div>
-                <div className="wrap row  justify-content-md-center">
+                <div className="wrap row  justify-content-center">
                     <div className="col-6 photo-tool">
                         <p>photo</p>
                     </div>
@@ -32,7 +32,7 @@ class App extends React.Component {
                         <p>layer</p>
                     </div>
                 </div>
-                <div className="wrap row  justify-content-md-center">
+                <div className="wrap row  justify-content-center">
                     <div className="col-10 file-manager">
                         <p>file</p>
                     </div>
@@ -43,3 +43,31 @@ class App extends React.Component {
 }
 
 export default App
+
+{/* <div class="row">
+  <div class="col-md-4">.col-md-4</div>
+  <div class="col-md-4 offset-md-4">.col-md-4 .offset-md-4</div>
+</div>
+<div class="row">
+  <div class="col-md-3 offset-md-3">.col-md-3 .offset-md-3</div>
+  <div class="col-md-3 offset-md-3">.col-md-3 .offset-md-3</div>
+</div>
+<div class="row">
+  <div class="col-md-6 offset-md-3">.col-md-6 .offset-md-3</div>
+</div> 
+
+mr-auto for margin
+
+<div class="row">
+  <div class="col-sm-9">
+    Level 1: .col-sm-9
+    <div class="row">
+      <div class="col-8 col-sm-6">
+        Level 2: .col-8 .col-sm-6
+      </div>
+      <div class="col-4 col-sm-6">
+        Level 2: .col-4 .col-sm-6
+      </div>
+    </div>
+  </div>
+</div>*/}
