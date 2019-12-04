2a2a2d36ec1fe18b725b527db9108d8fc13de0ba
baran <fabanaza@gmail.com>
2019-12-04 15:37:04 +0330

    modify photo dimensions

--- photomerger/src/components/photo-tool/photo-tool.js
+++ photomerger/src/components/photo-tool/photo-tool.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import photo from '../../assets/photoSample.jpg';
+// import photo from '../../assets/photoSample.jpg';
+import photo from '../../assets/photoSample2.jpg';
+//import photo from '../../assets/photoSample3.jpg';
+//import photo from '../../assets/photoSample4.jpg';
+// import photo from '../../assets/photoSample5.jpg';
 //const photo = require('./heif.jpg')
 
 class PhotoTool extends React.Component {
@@ -16,8 +20,22 @@ class PhotoTool extends React.Component {
         const ctx = canvas.getContext("2d")
         const img = this.refs.image
 
+
         img.onload = () => {
-            ctx.drawImage(img, 10, 10)
+            
+            var photoRatio = img.width / img.height;
+            var canvasRatio = canvas.width / canvas.height
+            if (photoRatio <= canvasRatio) {
+                ctx.drawImage(img, 0, 0, img.width*(canvas.height/img.height), canvas.height)
+            }
+            //if photow/h <= canvasw/h fit photo height to canvas height
+            //else fit photo width to canvas width
+            else {
+                ctx.drawImage(img, 0, 0, canvas.width, img.height*(canvas.width/img.width))
+            }
+
+
+
         }
 
     }
@@ -29,7 +47,7 @@ class PhotoTool extends React.Component {
         return (
             <div className="col-6 photo-tool">
                 <canvas id="my-canvas" ref="mycanvas" className="my-canvas">
-                    <img ref="image" src = {photo} className="photo-sample" />
+                    <img ref="image" src={photo} className="photo-sample" />
                 </canvas>
             </div>
         )
